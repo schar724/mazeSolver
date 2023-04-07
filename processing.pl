96981ad3b7ecb8c7247559c9e34afb2a81bf2d73
schar724 <scotty.charles@gmail.com>
2023-04-07 15:43:10 -0600

    changed file names

--- processing.pl
+++ processing.pl
@@ -5,7 +5,7 @@
    nodes to avoid infinite loops. */
 path(_,Node,Node,[Node], [Node|_]).
 path(Maze, StartNode, EndNode, [StartNode|Rest], Visited):-
-    e(Maze, StartNode,NextNode),
+    edge(Maze, StartNode,NextNode),
     not(member(NextNode, Visited)),append([NextNode], Visited, NewVisited),
     path(Maze, NextNode,EndNode, Rest, NewVisited).
 
@@ -18,7 +18,7 @@ node(Maze, n(X,Y,S,E)):-
     ;N is 9, S is 0,E is 1).
 
 % The e/3 predicate checks if two nodes are adjacent.
-e(Maze,n(X1,Y1,S1,E1),n(X2,Y2,S2,E2)):-
+edge(Maze,n(X1,Y1,S1,E1),n(X2,Y2,S2,E2)):-
     node(Maze,n(X1,Y1,S1,E1)),node(Maze,n(X2,Y2,S2,E2)),n(X1,Y1,S1,E1) \= n(X2,Y2,S2,E2),
     (X1 = X2, (Y1 is Y2+1;Y1 is Y2-1);
      Y1 = Y2, (X1 is X2+1;X1 is X2-1)).
